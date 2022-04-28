2a7dec4c3a165a301de2a9ccad9fb90eb0f70179
Benjamin <benjaminevans316@gmail.com>
2022-04-28 13:19:31 +0200

    backup computer

--- mapping.py
+++ mapping.py
@@ -701,10 +701,10 @@ def run_pre_map():
     fname = "config_test"
     conf = lib.load_conf(fname)
     # map_name = "porto"
-    map_name = "levine_blocked"
+    # map_name = "levine_blocked"
     # map_name = "columbia_small"
     # map_name = "f1_aut_wide"
-    # map_name = "berlin"
+    map_name = "berlin"
     
 
     pre_map = PreMap(conf, map_name)
